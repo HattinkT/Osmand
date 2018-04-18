66ac585bd61440e0a4258295fc8f1a9c7b153fd9
Tjalling Hattink <tjalling@hattink.net>
2018-04-18 16:39:37 +0200

    Always rotate to 0 degrees when resetting rotation

--- OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
+++ OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
@@ -402,6 +402,12 @@ public class OsmandMapTileView implements IMapDownloaderCallback {
 		}
 	}
 
+	public void resetRotate() {
+		if (getRotate() != 0) {
+			animatedDraggingThread.startRotate(0);
+		}
+	}
+
 	public boolean isShowMapPosition() {
 		return showMapPosition;
 	}
